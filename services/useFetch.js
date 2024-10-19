e7f040bd72745c79a5bc1652a6837460a969611b
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-10-20 00:44:08 +0300

    add: update useFetch

--- services/useFetch.js
+++ services/useFetch.js
@@ -1,6 +1,6 @@
 // app/services/useFetch.js
 
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 /**
@@ -8,8 +8,8 @@ const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
  * @param {string} endpoint - The API endpoint to call
  * @returns {Promise<Object>} - The response data
  */
-export const fetchData = async (endpoint) => {
-  const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}`;
+export const fetchData = async (endpoint, page = 1) => {
+  const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}&page=${page}`;
 
   try {
     const response = await fetch(url);
@@ -21,7 +21,7 @@ export const fetchData = async (endpoint) => {
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Fetch error:', error);
+    console.error("Fetch error:", error);
     throw error;
   }
 };
